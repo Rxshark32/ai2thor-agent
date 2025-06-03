fda90a55b4954525ccb018dbe73e7e62a8a188f1
Rxshark32 <rxshark2008@gmail.com>
2025-06-03 11:28:19 -0700

    Navigates to Stovetop and turns on Stoves based off toggleable objects

--- Controller.py
+++ Controller.py
@@ -1,4 +1,5 @@
 from ai2thor.controller import Controller
+import time
 
 controller = Controller(
     agentMode="default",
@@ -22,11 +23,15 @@ controller = Controller(
 
 def exploreRoom():
     controller.step("RotateLeft")
+    time.sleep(1)
     for i in range(10):
         controller.step("MoveAhead")
+        time.sleep(1)
     controller.step("RotateLeft")
+    time.sleep(1)
     for i in range(8):
         controller.step("MoveAhead")
+        time.sleep(1)
     controller.step("RotateLeft")
     controller.step("MoveAhead")
     controller.step("RotateRight")
@@ -39,8 +44,35 @@ def exploreRoom():
     for i in range(10):
         controller.step("MoveAhead")
 
-exploreRoom()
+def navStove():
+    controller.step("RotateLeft")
+    for i in range(10):
+        controller.step("MoveAhead")
+    controller.step("RotateLeft")
+    for i in range(3):
+        controller.step("MoveAhead")
+    controller.step("RotateRight")
+    controller.step("LookDown")
+    controller.step("MoveAhead")
+    controller.step("MoveAhead")
+    if not controller.step("MoveAhead").metadata["lastActionSuccess"]:
+        print("Final MoveAhead failed")
+        controller.step("MoveAhead")
 
-input("Press Enter to exit.")
+def turnOnStove():
+    event = controller.step(action="Pass")
+    for obj in event.metadata["objects"]:
+        if (obj["visible"] and (obj["objectType"] == "StoveKnob")):
+            print(obj["objectId"], obj["objectType"], obj["position"])
+            time.sleep(2)
+            controller.step(action="ToggleObjectOn", objectId=obj["objectId"])
+            time.sleep(2)
 
+
+# exploreRoom()
+navStove()
+turnOnStove()
+
+
+input("Press Enter to break.")
 controller.stop()

177d39edd551f7ba4944061fe555fac98a2ad5ab
Rxshark32 <rxshark2008@gmail.com>
2025-06-04 09:48:25 -0700

    Created method to show reachable positions and objects locations

--- Controller.py
+++ Controller.py
@@ -1,4 +1,6 @@
 from ai2thor.controller import Controller
+
+import matplotlib.pyplot as plt
 import time
 
 controller = Controller(
@@ -68,11 +70,37 @@ def turnOnStove():
             controller.step(action="ToggleObjectOn", objectId=obj["objectId"])
             time.sleep(2)
 
+def showMap():
+    positions = controller.step(action="GetReachablePositions")
+    x_vals = [pos['x'] for pos in positions.metadata["actionReturn"]]
+    z_vals = [pos['z'] for pos in positions.metadata["actionReturn"]]
+
+    # Made a temp list to store objects labels
+    objlist = []
+    for obj in positions.metadata['objects']:
+        objlist.append((obj['objectType'], obj['position']))
+
+    x_values = [obj[1]['x'] for obj in objlist]
+    z_values = [obj[1]['z'] for obj in objlist]
+    labels = [obj[0] for obj in objlist]
+
+    # Plot maker
+    plt.figure(figsize=(8, 8))
+    plt.xlim(-2.5, max(x_values) + 1)
+    plt.ylim(min(z_values)-0.5, max(z_values)+0.5)  
+    plt.scatter(x_vals, z_vals, c='blue', marker='s')
+    for i, label in enumerate(labels):
+        plt.annotate(label, (x_values[i], z_values[i]), textcoords="offset points", xytext=(5,5), ha='left')
+    plt.title("Reachable Positions (x-z grid)")
+    plt.xlabel("X")
+    plt.ylabel("Z")
+    plt.grid(True)
+    plt.gca().set_aspect('equal', adjustable='box')
+    plt.show()
+
 
 # exploreRoom()
-navStove()
-turnOnStove()
+# navStove()
+# turnOnStove()
+showMap()
 
-
-input("Press Enter to break.")
-controller.stop()
